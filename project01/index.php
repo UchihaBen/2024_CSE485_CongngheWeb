e12181e6f3a0100d70fdceabc1cdb95d1742bd48
hue1012 <doanthithuhue12n@gmail.com>
2024-02-23 11:07:29 +0700

    Huehue

--- project01/index.php
+++ project01/index.php
@@ -1,4 +1,18 @@
 <?php
-echo "ahaghd";
-echo"hurhur";
+    $navItems = [
+ "GIỚI THIỆU",
+ "TIN TỨC & THÔNG BÁO",
+ "TUYỂN SINH",
+ "ĐÀO TẠO",
+ "NGHIÊN CỨU",
+ "ĐỐI NGOẠI",
+ "VĂN BẢN",
+ "SINH VIÊN",
+ "LIÊN HỆ"
+];
+echo '<nav><ul>';
+foreach ($navItems as $item) {
+ echo "<li>$item</li>";
+}
+echo '</ul></nav>';
 ?>
